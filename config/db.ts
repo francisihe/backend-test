f24522b7e6c9d515e2d123791200d797dbff3de1
francisihe <francis.ihejirika@gmail.com>
2025-01-10 20:18:01 +0100

    config: updated db connection style to use either url or other connection options

--- config/db.ts
+++ config/db.ts
@@ -4,6 +4,7 @@ import { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DATABASE_URL } from '.
 let sequelize: Sequelize;
 
 if (DATABASE_URL) {
+    console.info('Using connection URL');
     // Use connection URL if available
     sequelize = new Sequelize(DATABASE_URL, {
         dialect: 'postgres',
@@ -16,6 +17,7 @@ if (DATABASE_URL) {
         }
     });
 } else {
+    console.info('Using credentials in constants.ts');
     // Use credentials in constants.ts if connection URL is not available
     sequelize = new Sequelize(DB_NAME as string, DB_USER as string, DB_PASSWORD as string, {
         host: DB_HOST as string,
